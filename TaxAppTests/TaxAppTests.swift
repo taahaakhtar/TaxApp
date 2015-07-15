cfdd4b89ac98041d14089f2252ef19206cc6c7b3
taahaakhtar <taahaakhtar@gmail.com>
2015-07-15 09:22:38 -0400

    Initial Commit

---
+++ TaxAppTests/TaxAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TaxAppTests.swift
+//  TaxAppTests
+//
+//  Created by Taaha Akhtar on 7/15/15.
+//  Copyright © 2015 Taaha Akhtar. All rights reserved.
+//
+
+import XCTest
+@testable import TaxApp
+
+class TaxAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
