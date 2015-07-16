e0fac68d874be2157a7cbe6794301d39338ea12b
taahaakhtar <taahaakhtar@gmail.com>
2015-07-16 16:28:32 -0400

    Ensured only two digits are entered after decimal place. Ensured project is only in portrait mode. Made auto layout features better.

--- TaxApp/ViewController.swift
+++ TaxApp/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var initialValueField: UITextField!
     @IBOutlet weak var salesTaxRateField: UITextField!
     @IBOutlet weak var discountRateField: UITextField!
+    @IBOutlet weak var finalDisplayedLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,38 +58,56 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let newLengthSalesTax = (salesTaxRateField.text!.characters.count) + (string.characters.count) - range.length
         
         let newLengthDiscountRate = (discountRateField.text!.characters.count) + (string.characters.count) - range.length
-        
-        
+
         // Limits decimal to one decimal point
-       let countdots = textField.text!.componentsSeparatedByString(".").count - 1
+       let countDots = textField.text!.componentsSeparatedByString(".").count - 1
         
-        if countdots > 0 && string == "."
+        if countDots > 0 && string == "."
         {
             return false
         }
         
+        // Limits digits after decimal to two digits
+        if textField.text!.rangeOfString(".") != nil {
+            if textField.text!.componentsSeparatedByString(".")[1].characters.count + string.characters.count > 2 {
+                return false
+            }
+        }
+
         // Returns true and returns maximum length allowed in text fields
         return true && newLengthInitialValue <= 8 && newLengthSalesTax <= 5 && newLengthDiscountRate <= 5 // Bool
         
     }
     
-        
     // MARK: Actions 
     
     @IBAction func setInitialValue(sender: UITextField) {
-    
+        
+//        var integerFinalDisplayed: Double? = Double(finalDisplayedLabel.text!)
+//        var integerInitialDisplayed: Double? = Double(initialValueField.text!)
+//    
+//        if integerInitialDisplayed == nil {
+//            integerFinalDisplayed = 0.00
+//        }
+//        
+//        else {
+        
+        finalDisplayedLabel.text = initialValueField.text
+        
         
     }
     
     @IBAction func setSalesTax(sender: UITextField) {
+        
     }
    
-    
+
     @IBAction func setDiscountRate(sender: UITextField) {
+        
     }
 
-    
+}
     
 
-}
+
 
